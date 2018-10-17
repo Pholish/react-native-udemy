9fc3fe15c51fc2a327e59d151671148fc107e357
Rakheb <Rogue.Pholish@gmail.com>
2018-10-17 18:15:32 +0300

    Added TabBased Navigation with Icons

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { Navigation } from 'react-native-navigation';
 import AuthScreen from './src/screens/Auth/Auth';
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 //Register Screens
-Navigation.registerComponent('awesome-places.appName', () => AuthScreen);
+Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
+Navigation.registerComponent(
+    'awesome-places.SharePlaceScreen',
+    () => SharePlaceScreen,
+);
+Navigation.registerComponent(
+    'awesome-places.FindPlaceScreen',
+    () => FindPlaceScreen,
+);
 
 //Start a App
 Navigation.startSingleScreenApp({
     screen: {
-        screen: 'awesome-places.appName',
+        screen: 'awesome-places.AuthScreen',
         title: 'Login',
     },
 });
