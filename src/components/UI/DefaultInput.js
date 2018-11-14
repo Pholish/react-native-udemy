e0f4a8c757f56ea0399125b66fb89a38a7aaef9f
Rakheb <Rogue.Pholish@gmail.com>
2018-11-14 19:10:23 +0200

    Added keyboard padding, auth action, switch mode, validation

--- src/components/UI/DefaultInput.js
+++ src/components/UI/DefaultInput.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { StyleSheet, TextInput } from 'react-native';
 
 const defaultInput = props => (
-    <TextInput {...props} style={[styles.input, props.style]} underlineColorAndroid="transparent" />
+    <TextInput
+        {...props}
+        style={[styles.input, props.style, !props.valid && props.touched ? styles.invalid : null]}
+        underlineColorAndroid="transparent"
+    />
 );
 const styles = StyleSheet.create({
     input: {
@@ -12,6 +16,10 @@ const styles = StyleSheet.create({
         padding: 5,
         marginTop: 8,
         marginBottom: 8,
+    },
+    invalid: {
+        backgroundColor: '#f9c0c0',
+        borderColor: 'red',
     },
 });
 
